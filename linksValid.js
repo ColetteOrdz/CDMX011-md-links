6c6d2a59d7aae086de9ac1e2cfedab01e2c7e480
ColetteOrdz <pcoletteor@gmail.com>
2021-10-19 14:16:06 -0500

    correct validate function

--- linksValid.js
+++ linksValid.js
@@ -1,31 +1,25 @@
 const axios = require('axios')
 
 async function validate(arrayLinks){
-    const arraylinks = {};  
+    //const array = {};  
     return await Promise.all(arrayLinks.map(async (link) => { 
         
     return await axios.get(link.href) 
         .then(response => {
             link.status = response.status;
             link.message = (link.status == 200) ? 'OK' : 'FAIL';
-            
+
+           // ----- CORREGIR EL RETURN, DEBE SER UN OBJETO ----- // 
             return `${link.file} ${link.href} ${link.message} ${link.status} ${link.text}`
-            /*arrayLinks.push({
-                message: link.message,
-                status: link.status
-            })
-            */
+            
         })
         .catch(error => {
             link.status = 404;
             link.message = 'FAIL';
-           
+            
+           // ----- CORREGIR EL RETURN, DEBE SER UN OBJETO ----- // 
             return `${link.file} ${link.href} ${link.message} ${link.status} ${link.text}`
-            /*arrayLinks.push({
-                message: link.message,
-                status: link.status
-            })
-            */
+            
         });
         
     }));
