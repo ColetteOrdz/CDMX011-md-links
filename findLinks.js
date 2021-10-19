4a3ef27569b5c318c00e657906c801d70c60fd15
ColetteOrdz <pcoletteor@gmail.com>
2021-10-18 21:01:40 -0500

    sync function to obtain md files and get Links function with marked

--- findLinks.js
+++ findLinks.js
@@ -1,26 +1,25 @@
-const fs = require('fs')
-const path = require('path')
-const markdownLinkExtractor = require('markdown-link-extractor');
-const arrayFiles = 'C:/Users/Colette/Desktop/Labo/CDMX011-md-links/README.md';
+const marked = require("marked");
+const fs = require("fs");
+const arrayFiles = 'C:/Users/Colette/Desktop/Labo/CDMX011-md-links/README.md'
 
-function findLinks (arrayFiles) {
-   
-    const markdown = fs.readFileSync(arrayFiles, {encoding: 'utf8'});
-    const arrayLinks = [] ;
-    const getLinks = markdownLinkExtractor(markdown, false);
-    getLinks.forEach((href, title, text) =>{
-        if(href.startsWith('#') === false){
-            arrayLinks.push({
-                href: href,
-                title: title.toString().slice(0,50),
-                arrayFiles: arrayFiles,
-            });
-        }
-    }); 
-        
-    console.log(arrayLinks)  
+function getLinks (mdfile) {
+    const arraylinks = [];
+    const list = fs.readFileSync(mdfile).toString();
+    const render = new marked.Renderer();
+        render.link = (href, title, text) => {
+            if (href.startsWith("#") === false) {
+                arraylinks.push({
+                    href: href,
+                    text: text,
+                    mdfile: mdfile,
+                });
+            }
+        };
+  marked(list, { renderer: render });
+  console.log(arraylinks);
+  return arraylinks
+};
 
-   //return arrayLinks
-}
-findLinks(arrayFiles)
-//module.exports = findLinks 
+getLinks(arrayFiles);
+
+//exports.getLinks = getLinks
